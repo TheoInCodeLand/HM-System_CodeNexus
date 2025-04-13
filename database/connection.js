6590f4a6faa325c8f096ffa00080a1625657ea98
Theophilus Thobejane (220330220) <Thobejanetheo@gmail.com>
2025-04-13 23:42:34 +0200

    Database setup

---
+++ database/connection.js
@@ -0,0 +1,8 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database/Hotel.db', (err)=> {
+    if (err) {
+        console.error(err.message, 'Failed to connect to the hotel database!');
+    } else {
+        console.log('Connected to the hotel database.');
+    }
+})
